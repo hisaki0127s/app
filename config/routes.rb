7c2a12191f7ca4b52e0f76a6dda89992e9924288
hisaki0127s <hisaki3620@gmail.com>
2023-02-09 22:12:08 +0900

    自由にわかりやすいメッセージ

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,19 @@
 Rails.application.routes.draw do
+
+
+
+  get 'muscles/men5' => 'muscles#men5'
+  get 'muscles/men6' => 'muscles#men6'
+  get 'muscles/women5' => 'muscles#women5'
+  get 'muscles/women6' => 'muscles#women6'
+  
   devise_for :users, controllers: {
     registrations: 'users/registrations',
     sessions: 'users/sessions'
   }
 
   
-  get 'muscles/men5' => 'muscles#men5'
-  get 'muscles/men6' => 'muscles#men6'
-  get 'muscles/women5' => 'muscles#women5'
-  get 'muscles/women6' => 'muscles#women6'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: [:show]
   root 'tests#index'
